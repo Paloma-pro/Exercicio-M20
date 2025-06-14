7606fe2ae68ad1ab09fb34a5e3dfd95566dda721
Palomadev <paloma166.pah@gmail.com>
2025-06-14 12:57:22 -0300

    Adição dos arquivos .js da atividade

---
+++ Atividade3.js
@@ -0,0 +1,12 @@
+function somaMultiplos(limite) {
+  let soma = 0;
+  for (let i = 1; i < limite; i++) {
+    if (i % 5 === 0 || i % 7 === 0) {
+      soma += i;
+    }
+  }
+  return soma;
+}
+
+const resultado = somaMultiplos(1000);
+console.log(resultado);
